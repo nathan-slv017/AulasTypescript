e9d637a2428c1352fcab18d5cdf5f1ce466dea46
Nathan silva <email.do.natann@gmail.com>
2021-08-30 18:15:10 -0300

    30/08

--- src/oi/trino-de-28.ts
+++ src/oi/trino-de-28.ts
@@ -1,1 +1,39 @@
-export class Pessoa {}
+export class Pessoa {
+    constructor(
+        public nome: string,
+        public sobrenome: string,
+        private idade: number,
+        protected cpf: string
+    ) {}
+
+    getIdade(): number {
+        return this.idade;
+    }
+    getNomeCompleto(): string {
+        return this.nome + " " + this.sobrenome;
+    }
+    getCod(): string {
+        return this.cpf;
+    }
+}
+
+export class Aluno extends Pessoa {
+    getNomeCompleto(): string {
+        return "Nome do aluno(a) é " + this.nome + " " + this.sobrenome;
+    }
+}
+
+export class Empregado extends Pessoa {
+    getNomeCompleto(): string {
+        return "Nome do empregado(a) é " + this.nome + " " + this.sobrenome;
+    }
+}
+
+const pessoa = new Pessoa("Nathan", "Silva", 19, "102910290102");
+console.log(pessoa.getNomeCompleto());
+
+const aluno = new Aluno("Vitoria", "Ana", 17, "1039039292");
+console.log(aluno.getNomeCompleto());
+
+const empregado = new Pessoa("Matheus", "Ribeiro", 22, "39023992839");
+console.log(empregado.getNomeCompleto());
